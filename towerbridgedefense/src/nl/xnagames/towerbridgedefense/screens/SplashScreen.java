e876a6053ff28ee833d6645840744a29c46838b2
Arjan de Ruijter <rra@mboutrecht.nl>
2014-02-13 09:27:47 +0100

    Initial commit

---
+++ towerbridgedefense/src/nl/xnagames/towerbridgedefense/screens/SplashScreen.java
@@ -0,0 +1,70 @@
+package nl.xnagames.towerbridgedefense.screens;
+
+import nl.xnagames.towerbridgedefense.TowerBridgeDefense;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL10;
+
+public class SplashScreen implements Screen
+{
+	// Fields
+	
+	
+	
+	
+	// Properties
+	
+	
+	
+	// Constructor
+	public SplashScreen(TowerBridgeDefense game)
+	{
+		
+	}
+	
+	
+	@Override
+	public void render(float delta)
+	{
+		Gdx.gl.glClearColor(0f, 0.19f, 0.19f, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+	}
+
+	@Override
+	public void resize(int width, int height) 
+	{
+		
+	}
+
+	@Override
+	public void show() 
+	{
+		
+	}
+
+	@Override
+	public void hide() 
+	{
+		
+	}
+
+	@Override
+	public void pause() 
+	{
+		
+	}
+
+	@Override
+	public void resume() 
+	{
+		
+	}
+
+	@Override
+	public void dispose() 
+	{
+		
+	}
+	
+}
