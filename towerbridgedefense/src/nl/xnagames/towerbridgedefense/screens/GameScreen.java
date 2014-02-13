6f2e6a3c3eb4c35dd6750e9bbc456236f05dd8d4
Arjan de Ruijter <rra@mboutrecht.nl>
2014-02-13 10:12:51 +0100

    GameScreen class gemaakt, we kunnen nu schakelen tussen gametoestanden

--- towerbridgedefense/src/nl/xnagames/towerbridgedefense/screens/GameScreen.java
+++ towerbridgedefense/src/nl/xnagames/towerbridgedefense/screens/GameScreen.java
@@ -3,11 +3,10 @@ package nl.xnagames.towerbridgedefense.screens;
 import nl.xnagames.towerbridgedefense.TowerBridgeDefense;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
 
-public class SplashScreen implements Screen
+public class GameScreen implements Screen
 {
 	// Fields
 	private TowerBridgeDefense game;
@@ -18,59 +17,48 @@ public class SplashScreen implements Screen
 	
 	
 	// Constructor
-	public SplashScreen(TowerBridgeDefense game)
+	public GameScreen(TowerBridgeDefense game)
 	{
 		this.game = game;
+		
 	}
 	
 	
+	
 	@Override
-	public void render(float delta)
-	{
-		Gdx.gl.glClearColor(1f, 0f, 0f, 1f);
+	public void render(float delta) 
+	{	
+		Gdx.gl.glClearColor(0f, 1f, 0f, 1f);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
-		// Door het indrukken van de spatiebalk ga je naar de gameScreen toestand
-		if ( Gdx.input.isKeyPressed(Keys.SPACE))
-		{
-			this.game.setScreen(this.game.getGameScreen());			
-		}
 	}
 
 	@Override
-	public void resize(int width, int height) 
-	{
-		
+	public void resize(int width, int height)
+	{		
 	}
 
 	@Override
 	public void show() 
-	{
-		
+	{		
 	}
 
 	@Override
 	public void hide() 
-	{
-		
+	{		
 	}
 
 	@Override
-	public void pause() 
-	{
-		
+	public void pause()
+	{		
 	}
 
 	@Override
 	public void resume() 
-	{
-		
+	{		
 	}
 
 	@Override
 	public void dispose() 
-	{
-		
+	{		
 	}
-	
 }
